f34cffc92d34909ec5fb7385827392ca0b0ff538
Evgeny Kitaev <evkitaev@gmail.com>
2018-05-24 08:19:10 +0300

    c3 from intellij

--- src/com/first/com/second/SecondClass.java
+++ src/com/first/com/second/SecondClass.java
@@ -2,7 +2,7 @@ package com.first.com.second;
 
 public class SecondClass {
     public static void print() {
-        System.out.printf("Hello World!!");
+        System.out.printf("Domashechka!)");
     }
 
 
