f593ae8ffc87c9c53dbc57994b0766c80ff5bc5e
Evgeny Kitaev <evkitaev@gmail.com>
2018-05-26 16:51:35 +0300

    c4 + branch create lesson3

--- src/com/first/com/second/SecondClass.java
+++ src/com/first/com/second/SecondClass.java
@@ -2,7 +2,7 @@ package com.first.com.second;
 
 public class SecondClass {
     public static void print() {
-        System.out.printf("Domashechka!)");
+        System.out.printf("Domashechka!!)");
     }
 
 
